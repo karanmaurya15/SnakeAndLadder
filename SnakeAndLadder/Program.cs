72c5ef978b2f562a175f2eb355942e5286b475c4
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-02 14:13:40 +0530

    Wellcome Program

---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome TO SNAKE AND LADDER Game");
+        }
+    }
+}
