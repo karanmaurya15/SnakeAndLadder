16f6559db7925ee38ee4491e3c701329a88f42f2
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-02 14:24:32 +0530

    Added Snake and Ladder

---
+++ SnakeAndLadder/SnakeAndLadder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder
+    {
+        public int PlayerPosition = 0;
+    }
+}
